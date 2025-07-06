1e19c92bcb4d9ada49cd2a09f8aea9b4ad8de1b4
Denis Danilov <d.danilov@duotek.ru>
2025-07-06 13:43:14 +0300

    Hero section #2

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -1,7 +1,43 @@
 <script setup lang="ts">
 import Menu from '~/components/Menu.vue'
+import Button from '~/components/Button.vue'
+
+const tags = ref([
+    'ORM',
+    'SERM',
+    'Репутация с нуля',
+    'Мониторинг',
+    'Создание позитивного контента'
+])
 </script>
 
 <template>
-  <Menu />
+  <section class="hero">
+    <Menu />
+    <div class="hero__container">
+      <div class="hero__left">
+        <h1>
+          <span>Управление</span>
+          <span>репутацией</span>
+          <span>в интернете</span>
+        </h1>
+        <div class="hero__tags">
+          <div v-for="tag in tags">
+            {{ tag }}
+          </div>
+        </div>
+        <div class="hero__button-container">
+          <Button>
+            Получить бесплатный SERM аудит
+          </Button>
+          <Button class="--type-login">
+            <svg><use :href="'/sprite.svg#login'" /></svg>
+          </Button>
+        </div>
+      </div>
+      <div class="hero__right">
+        <!--        -->
+      </div>
+    </div>
+  </section>
 </template>

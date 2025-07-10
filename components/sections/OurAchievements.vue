957388ae12a4bc0e8dcf0b399fa1bd8d69c1589a
Denis Danilov <d.danilov@duotek.ru>
2025-07-10 20:13:27 +0300

    Our achievements #2

--- components/sections/OurAchievements.vue
+++ components/sections/OurAchievements.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
-
+const achievements = ref([
+  {
+    left: '20+',
+    right: 'Вывод 20+ брендов в ТОП-10 поисковой выдачи'
+  },
+  {
+    left: '200',
+    right: 'Устранение более 200 единиц негатива из поисковых систем'
+  },
+  {
+    left: 'Топ-3',
+    right: 'Топ-3 среди агентств по управлению репутацией в области образования'
+  },
+  {
+    left: '4.9 из 5',
+    right: 'Средний рейтинг удовлетворенности клиентов — 4.9 из 5'
+  },
+])
 </script>
 
 <template>
   <section class="our-achievements">
     <TagWithLabel class="--alternative-rotate" icon="award">#победители</TagWithLabel>
     <h2>Наши достижения</h2>
+    <div class="our-achievements__achievements">
+      <div v-for="(achievement, i) in achievements">
+        <div>{{ ++i }}</div>
+        <svg><use :href="'/sprite.svg#done'" /></svg>
+        <div>{{ achievement.left }}</div>
+        <div>{{ achievement.right }}</div>
+      </div>
+    </div>
   </section>
 </template>

d861abfa8fbd9ce5f8a54c777032b876e64fe00c
Denis Danilov <d.danilov@duotek.ru>
2025-07-14 07:33:17 +0300

    Footer #1

--- components/sections/Footer.vue
+++ components/sections/Footer.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
-
+import LogoAlt from '~/assets/svg/logo-alt.svg?component'
 </script>
 
 <template>
-  <div style="margin-top: 50px;">
-    123
-  </div>
+  <section class="footer">
+    <div class="footer__container">
+      <div class="footer__column">
+        <div class="footer__logo-container">
+          <LogoAlt />
+          <div>Digital Dyatel</div>
+        </div>
+        <div>© 2024-2025 Digital Dyatel</div>
+        <div>
+          <span>Управление репутацией. Все права защищены.</span>
+          <span>При копировании информации ссылка на источник обязательна</span>
+        </div>
+      </div>
+      <div class="footer__column">
+        <a href="/">Позвоните мне</a>
+        <a>8 800 533-82-43</a>
+        <div>Бесплатно по России</div>
+        <a>+7 495 145-30-53</a>
+        <div>Бесплатно по Москве</div>
+      </div>
+      <div>
+        <div>info@digitaldyatel.ru</div>
+        <a>Написать в WhatsApp</a>
+        <a>Написать в Telegram</a>
+      </div>
+      <div>
+        <div>г. Москва, Дербеневская набережная, д.11</div>
+        <div>Режим работы: будние дни 10:00–19:00</div>
+      </div>
+    </div>
+  </section>
 </template>

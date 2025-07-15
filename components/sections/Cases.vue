49f67759a8e96ecf18cf457a3e4d0714c5828f82
Denis Danilov <d.danilov@duotek.ru>
2025-07-15 16:03:21 +0300

    FAQ #3

--- components/sections/Cases.vue
+++ components/sections/Cases.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
-import type { Ref } from 'vue'
+import { onMounted, type Ref } from 'vue'
 import { categories as _categories } from '~/constants'
+
+let progressBarTrackWidth: float | undefined = undefined
+let categoriesContainerWidth: float | undefined = undefined
+let ratio: float | undefined = undefined
+
+const translateX = ref(0)
+
+const progressBarTrackTemplateRef = useTemplateRef('progressBarTrackTemplateRef')
+const progressBarSliderTemplateRef = useTemplateRef('progressBarSliderTemplateRef')
+
+const categoriesContainerTemplateRef = useTemplateRef('categoriesContainerTemplateRef')
+const categoriesTemplateRef = useTemplateRef('categoriesTemplateRef')
 
 const categories: Ref<Category[]> = ref(_categories)
 
 const activeCategoryIndex = ref(0)
+const activeCategory = ref(null)
 
-const onClickCategory = (i: number) => {
+const setCategory = (i: number) => {
   activeCategoryIndex.value = i
+  activeCategory.value = categories.value[activeCategoryIndex.value]
+
+  updateDimensions(i)
 }
 
 const onClickPrev = () => {
@@ -16,6 +32,8 @@ const onClickPrev = () => {
   }
 
   activeCategoryIndex.value = --activeCategoryIndex.value
+  activeCategory.value = categories.value[activeCategoryIndex.value]
+  updateDimensions(activeCategoryIndex.value)
 }
 
 const onClickNext = () => {
@@ -24,7 +42,30 @@ const onClickNext = () => {
   }
 
   activeCategoryIndex.value = ++activeCategoryIndex.value
+  activeCategory.value = categories.value[activeCategoryIndex.value]
+  updateDimensions(activeCategoryIndex.value)
 }
+
+const updateDimensions = (index: number) => {
+  progressBarSliderTemplateRef.value.style.width = (progressBarTrackWidth / categories.value.length * (index + 1)) + 'px'
+
+  let totalWidth = 0
+
+  for (let i = 0; i < index; i++) {
+    totalWidth += categoriesTemplateRef.value[i].getBoundingClientRect().width + 24
+  }
+
+  translateX.value = totalWidth * -1
+}
+
+onMounted(() => {
+  progressBarTrackWidth = progressBarTrackTemplateRef.value.getBoundingClientRect().width
+  categoriesContainerWidth = categoriesContainerTemplateRef.value.getBoundingClientRect().width
+
+  ratio = progressBarTrackWidth / categoriesContainerWidth
+
+  setCategory(0)
+})
 </script>
 
 <template>
@@ -32,76 +73,98 @@ const onClickNext = () => {
     <h2>Нам доверили свою репутацию</h2>
     <p>Мы помогаем компаниям выстраивать, защищать и улучшать репутацию в интернете. Вот несколько примеров, как мы достигли результатов</p>
     <div class="cases__container">
-      <div class="cases__categories">
-        <div v-for="(category, i) in categories" :class="{'--active': i === activeCategoryIndex}" @click="onClickCategory(i)">
-          {{ category.title }}
+      <div class="cases__header-container">
+        <div class="cases__categories-container">
+          <div class="cases__categories" ref="categoriesContainerTemplateRef" :style="{transform: 'translateX(' + translateX + 'px)'}">
+            <div ref="categoriesTemplateRef" v-for="(category, i) in categories" :class="{'--active': i === activeCategoryIndex}" @click="setCategory(i)">
+              {{ category.title }}
+            </div>
+          </div>
+        </div>
+        <div class="cases__paginator-container">
+          <div class="cases__paginator-progress-bar">
+            <div class="cases__paginator-progress-bar-track" ref="progressBarTrackTemplateRef"></div>
+            <div class="cases__paginator-progress-bar-slider" ref="progressBarSliderTemplateRef"></div>
+          </div>
+          <div class="cases__paginator-controls">
+            <div @click="onClickPrev(i)"><svg><use :href="'/sprite.svg#chevron-left'" /></svg></div>
+            <div @click="onClickNext(i)"><svg><use :href="'/sprite.svg#chevron-right'" /></svg></div>
+          </div>
         </div>
       </div>
-      <div class="cases__paginator-container">
-        <div class="cases__paginator-progress-bar">
-          <div></div>
-          <div></div>
-        </div>
-        <div class="cases__paginator-controls">
-          <div @click="onClickPrev(i)"><svg><use :href="'/sprite.svg#chevron-left'" /></svg></div>
-          <div @click="onClickNext(i)"><svg><use :href="'/sprite.svg#chevron-right'" /></svg></div>
-        </div>
-      </div>
-      <div class="cases__active-category">
-        <template v-for="_case in categories[activeCategoryIndex].cases">
-          <div class="cases__case-container" v-if="_case instanceof Array">
-            <div class="cases__case" v-for="__case in _case">
-              <div class="cases__case-header">
-                <img :src="'/img/cases/' + __case.img" :alt="__case.header">
-                <div>{{ __case.category ?? categories[activeCategoryIndex].title }}</div>
-              </div>
-              <div class="cases__case-content">
-                <div>
-                  <div>{{ __case.header }}</div>
+      <div class="cases__active-category-container">
+        <transition name="slide-fade" mode="out-in">
+          <div class="cases__active-category" :key="activeCategoryIndex">
+            <template v-for="_case in categories[activeCategoryIndex].cases">
+              <div class="cases__case-container" v-if="_case instanceof Array">
+                <div class="cases__case" v-for="__case in _case">
+                  <div class="cases__case-header">
+                    <img :src="'/img/cases/' + __case.img" :alt="__case.header">
+                    <div>{{ __case.category ?? categories[activeCategoryIndex].title }}</div>
+                  </div>
+                  <div class="cases__case-content">
+                    <div>
+                      <div>{{ __case.header }}</div>
+                    </div>
+                    <div class="cases__case-description">
+                      <div>Задача</div>
+                      <p>{{ __case.task }}</p>
+                      <div>Результат</div>
+                      <p>{{ __case.result }}</p>
+                      <div>
+                        <div>{{ __case.startRating }} репутация</div>
+                        <svg><use :href="'/sprite.svg#arrow-right'" /></svg>
+                        <div>{{ __case.finishRating }} репутация</div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div class="cases__case-description">
-                  <div>Задача</div>
-                  <p>{{ __case.task }}</p>
-                  <div>Результат</div>
-                  <p>{{ __case.result }}</p>
+              </div>
+              <div v-else class="cases__case">
+                <div class="cases__case-header">
+                  <img :src="'/img/cases/' + _case.img" :alt="_case.header">
+                  <div>{{ _case.category ?? categories[activeCategoryIndex].title }}</div>
+                </div>
+                <div class="cases__case-content">
                   <div>
-                    <div>{{ __case.startRating }} репутация</div>
-                    <svg><use :href="'/sprite.svg#arrow-right'" /></svg>
-                    <div>{{ __case.finishRating }} репутация</div>
+                    <div>{{ _case.header }}</div>
+                    <div class="cases__case-tags">
+                      <div v-for="tag in _case.tags">
+                        {{ tag }}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="cases__case-description">
+                    <div>Задача</div>
+                    <p>{{ _case.task }}</p>
+                    <div>Результат</div>
+                    <p>{{ _case.result }}</p>
+                    <div>
+                      <div>{{ _case.startRating }} репутация</div>
+                      <svg><use :href="'/sprite.svg#arrow-right'" /></svg>
+                      <div>{{ _case.finishRating }} репутация</div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </template>
           </div>
-          <div v-else class="cases__case">
-            <div class="cases__case-header">
-              <img :src="'/img/cases/' + _case.img" :alt="_case.header">
-              <div>{{ _case.category ?? categories[activeCategoryIndex].title }}</div>
-            </div>
-            <div class="cases__case-content">
-              <div>
-                <div>{{ _case.header }}</div>
-                <div class="cases__case-tags">
-                  <div v-for="tag in _case.tags">
-                    {{ tag }}
-                  </div>
-                </div>
-              </div>
-              <div class="cases__case-description">
-                <div>Задача</div>
-                <p>{{ _case.task }}</p>
-                <div>Результат</div>
-                <p>{{ _case.result }}</p>
-                <div>
-                  <div>{{ _case.startRating }} репутация</div>
-                  <svg><use :href="'/sprite.svg#arrow-right'" /></svg>
-                  <div>{{ _case.finishRating }} репутация</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </template>
+        </transition>
       </div>
     </div>
   </section>
 </template>
+
+<style scoped>
+.slide-fade-enter-active, .slide-fade-leave-active {
+  transition: all 0.5s ease-out;
+}
+.slide-fade-enter-from {
+  transform: translateX(100%);
+  opacity: 0;
+}
+.slide-fade-leave-to {
+  transform: translateX(-100%);
+  opacity: 0;
+}
+</style>

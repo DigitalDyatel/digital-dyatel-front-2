d458e6e63d5565053f1ac96edc2434307e3e262b
Denis Danilov <d.danilov@duotek.ru>
2025-07-19 00:39:51 +0300

    Added typescript configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  typescript: {
+    typeCheck: false,
+    strict: false
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: false },
   css: ['~/assets/scss/app.scss'],

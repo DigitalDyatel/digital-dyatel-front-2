8bf19bcab664a89e208162c2d277336b8c4c3b02
Denis Danilov <d.danilov@duotek.ru>
2025-07-23 15:52:25 +0300

    Set user-scalable: 0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, user-scalable=0',
+        }
+      ]
+    }
+  },
   typescript: {
     typeCheck: true,
     strict: false

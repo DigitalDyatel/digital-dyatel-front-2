174cabea880592ec42fd2246b9c0273e79d9a5e9
Denis Danilov <d.danilov@duotek.ru>
2025-07-23 21:47:32 +0300

    Added menu toggle contact us button

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import Logo from '~/assets/svg/logo.svg?component'
 import Button from '~/components/Button.vue'
+import { onClickOutside } from '@vueuse/core'
 import { type CustomRuntimeConfig } from '~/types'
 
 const config = useRuntimeConfig() as CustomRuntimeConfig
+
+const contactUsIsOpen = ref(false)
+const contactUsTemplateRef = useTemplateRef<HTMLDivElement>('contactUsTemplateRef')
+
+let clickOutsideFunction: ReturnType<typeof onClickOutside> | undefined = undefined
 
 interface MenuItem {
   label: string,
@@ -29,6 +35,24 @@ const menuItems = ref<MenuItem[]>([
     label: 'Отзывы',
   },
 ])
+
+const toggleContactUs = () => {
+
+  if (contactUsIsOpen.value) {
+    contactUsIsOpen.value = false
+    clickOutsideFunction()
+    return
+  }
+
+  contactUsIsOpen.value = true
+
+  nextTick(() => {
+    clickOutsideFunction = onClickOutside(contactUsTemplateRef.value, () => {
+      clickOutsideFunction?.()
+      contactUsIsOpen.value = false
+    })
+  })
+}
 
 onMounted(() => {
 
@@ -59,19 +83,58 @@ onMounted(() => {
           </div>
         </div>
         <div class="menu__right">
-          <Button style="display: none;" class="--rounded --type-search">
-            <svg>
-              <use :href="'/sprite.svg#search'"/>
-            </svg>
-          </Button>
-          <Button class="--rounded --type-grey">
-            {{ config.public.email }}
-          </Button>
-          <Button class="--rounded">
-            Заказать звонок
-          </Button>
+          <div class="menu__button-group">
+            <Button style="display: none;" class="--rounded --type-search">
+              <svg>
+                <use :href="'/sprite.svg#search'"/>
+              </svg>
+            </Button>
+            <Button class="--rounded --type-grey">
+              {{ config.public.email }}
+            </Button>
+            <Button class="--rounded" @click="toggleContactUs">
+              Заказать звонок
+            </Button>
+          </div>
+          <transition name="fade-in">
+            <div ref="contactUsTemplateRef" class="menu__contact-us" v-if="contactUsIsOpen">
+              <div class="menu__contact-us-line">
+                <div class="menu__contact-us-header">Связаться с нами</div>
+                <svg @click="toggleContactUs"><use :href="'/sprite.svg#close'" /></svg>
+              </div>
+              <div class="menu__contact-us-line">
+                <div class="menu__contact-us-phones" v-for="phone in config.public.phones">
+                  <div class="menu__contact-us-phone">
+                    <div>{{ phone.phone }}</div>
+                    <div>{{ phone.description }}</div>
+                  </div>
+                </div>
+              </div>
+              <div class="menu__contact-us-line --social">
+                <Button>Обратный звонок</Button>
+                <div class="menu__contact-us-social-group">
+                  <Button class="--tertiary"><svg><use :href="'/sprite.svg#telegram'" /></svg></Button>
+                  <Button class="--tertiary"><svg><use :href="'/sprite.svg#whatsapp'" /></svg></Button>
+                </div>
+              </div>
+            </div>
+          </transition>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.fade-in-enter-active, .fade-in-leave-active {
+  transition: all .1s ease-out;
+}
+.fade-in-enter-from {
+  opacity: 0;
+  transform: rotateX(-30deg) scale(.9);
+}
+.fade-in-leave-to {
+  opacity: 0;
+  transform: rotateX(-30deg) scale(.9);
+}
+</style>

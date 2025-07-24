7215c875a27d82d18107ad50a5e38724f721bc72
Denis Danilov <d.danilov@duotek.ru>
2025-07-25 00:58:38 +0300

    Minor changes

--- components/Menu.vue
+++ components/Menu.vue
@@ -6,6 +6,7 @@ import { type CustomRuntimeConfig } from '~/types'
 
 const config = useRuntimeConfig() as CustomRuntimeConfig
 const notification = useNotification()
+const device = useDevice()
 const { goToAnchor } = useAnchor()
 
 const contactUsIsOpen = ref(false)
@@ -73,9 +74,29 @@ const onClickMenu = (menuItem: MenuItem) => {
   goToAnchor(menuItem.scrollSelector)
 }
 
-onMounted(() => {
+const clickOnPhone = async (phone) => {
+  if (device.isMobile()) {
+    const a = document.createElement('a')
+    a.href = 'tel:' + phone.phoneRaw
+    a.click()
+    a.remove()
+    return
+  }
 
-})
+  await navigator.clipboard.writeText(phone.phoneRaw)
+  notification.fire({
+    title: 'Телефон скопирован!',
+    class: 'izi-toast',
+  })
+}
+
+const createLinkAndFollow = (href: string) => {
+  const a = document.createElement('a')
+  a.target = '_blank'
+  a.href = href
+  a.click()
+  a.remove()
+}
 </script>
 
 <template>
@@ -125,7 +146,7 @@ onMounted(() => {
               <div class="menu__contact-us-line">
                 <div class="menu__contact-us-phones" v-for="phone in config.public.phones">
                   <div class="menu__contact-us-phone">
-                    <div>{{ phone.phone }}</div>
+                    <div @pointerdown="clickOnPhone(phone)" @contextmenu.prevent>{{ phone.phone }}</div>
                     <div>{{ phone.description }}</div>
                   </div>
                 </div>
@@ -133,8 +154,8 @@ onMounted(() => {
               <div class="menu__contact-us-line --social">
                 <Button>Обратный звонок</Button>
                 <div class="menu__contact-us-social-group">
-                  <Button class="--tertiary"><svg><use :href="'/sprite.svg#telegram'" /></svg></Button>
-                  <Button class="--tertiary"><svg><use :href="'/sprite.svg#whatsapp'" /></svg></Button>
+                  <Button class="--tertiary" @click="createLinkAndFollow(config.public.telegram)"><svg><use :href="'/sprite.svg#telegram'" /></svg></Button>
+                  <Button class="--tertiary" @click="createLinkAndFollow(config.public.whatsapp)"><svg><use :href="'/sprite.svg#whatsapp'" /></svg></Button>
                 </div>
               </div>
             </div>

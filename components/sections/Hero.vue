b85ffa2d4c9220ad0e7b3589225e38a699c0d42b
Denis Danilov <d.danilov@duotek.ru>
2025-08-08 13:10:50 +0300

    Temp

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -4,6 +4,19 @@ import FormModal from '~/components/modals/FormModal.vue'
 import ThankYouModal from '~/components/modals/ThankYouModal.vue'
 import { useModal } from 'vue-final-modal'
 
+const tagsTemplateRef = useTemplateRef('tagsTemplateRef')
+
+let isTouching = false;
+let startX = 0;
+let scrollStartX = 0;
+let lastX = 0;
+let velocity = 0;
+let lastTime = 0;
+
+let io: IntersectionObserver | undefined = undefined
+
+const isMobileTagsOverlayHidden = ref(false)
+
 const advantages = [
     'Стратегия под ваши цели',
     'Реальные отзывы',
@@ -36,6 +49,86 @@ const onClick = () => {
 
   open()
 }
+
+const touchStartEventListener = e => {
+  if (e.touches.length !== 1) return;
+
+  isTouching = true;
+  startX = e.touches[0].pageX;
+  scrollStartX = tagsTemplateRef.value.scrollLeft;
+  lastX = startX;
+  lastTime = Date.now();
+  velocity = 0;
+}
+
+const touchMoveEventListener = e => {
+  if (!isTouching || e.touches.length !== 1) return;
+
+  const currentX = e.touches[0].pageX;
+  const dx = currentX - startX;
+  tagsTemplateRef.value.scrollLeft = scrollStartX - dx;
+
+  const now = Date.now();
+  const deltaX = currentX - lastX;
+  const deltaTime = now - lastTime;
+  velocity = (deltaX / deltaTime) * 16;
+
+  lastX = currentX;
+  lastTime = now;
+}
+
+const touchEndEventListener = e => {
+  if (!isTouching) return;
+  isTouching = false;
+
+  const friction = 0.95;
+  function inertiaScroll() {
+    velocity *= friction;
+    tagsTemplateRef.value.scrollLeft -= velocity;
+
+    if (Math.abs(velocity) > 0.5) {
+      requestAnimationFrame(inertiaScroll);
+    }
+  }
+
+  if (Math.abs(velocity) > 1) {
+    requestAnimationFrame(inertiaScroll);
+  }
+}
+
+onMounted(() => {
+  if (window.innerWidth >= 768) {
+    return
+  }
+
+  tagsTemplateRef.value.addEventListener('touchstart', touchStartEventListener, { passive: true });
+  tagsTemplateRef.value.addEventListener('touchmove', touchMoveEventListener, { passive: true })
+  tagsTemplateRef.value.addEventListener('touchend', touchEndEventListener)
+
+  const io = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          isMobileTagsOverlayHidden.value = entry.isIntersecting
+        });
+      },
+      {
+        root: tagsTemplateRef.value,
+        threshold: 1.0,
+      }
+  );
+  io.observe(tagsTemplateRef.value.children[tags.value.length - 1])
+})
+
+onUnmounted(() => {
+  if (window.innerWidth >= 768) {
+    return
+  }
+
+  tagsTemplateRef.value.removeEventListener('touchstart', touchStartEventListener, { passive: true });
+  tagsTemplateRef.value.removeEventListener('touchmove', touchMoveEventListener, { passive: true })
+  tagsTemplateRef.value.removeEventListener('touchend', touchEndEventListener)
+  io.disconnect()
+})
 </script>
 
 <template>
@@ -48,8 +141,8 @@ const onClick = () => {
           <span>в интернете</span>
         </h1>
         <div class="hero__tags-container">
-          <div class="hero__tags-overlay" />
-          <div class="hero__tags">
+          <div class="hero__tags-overlay" :class="{'--hidden': isMobileTagsOverlayHidden}"/>
+          <div class="hero__tags" ref="tagsTemplateRef">
             <div v-for="tag in tags">
               {{ tag }}
             </div>

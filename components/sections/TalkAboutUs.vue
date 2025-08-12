77f9658c4d84e1f44fad74627fd992e26ff1dcd4
Denis Danilov <d.danilov@duotek.ru>
2025-08-12 17:53:19 +0300

    Temp

--- components/sections/TalkAboutUs.vue
+++ components/sections/TalkAboutUs.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import Link from '~/components/Link.vue'
 
+const swiperContainerTemplateRef = useTemplateRef('swiperContainerTemplateRef')
+const h2TemplateRef = useTemplateRef('h2TemplateRef')
+
+const isMobile = ref(false)
+const currentSlideIndex = ref(0)
+
 const mediaResources = ref([
   {
     img: 'm24.png',
@@ -19,21 +25,78 @@ const mediaResources = ref([
     alt: 'Сноб',
     isWithTag: true,
   },
+  {
+    alt: 'Все публикации',
+  },
 ])
+
+const mediaResourcesMobile = ref([
+  [
+    {
+      img: 'm24.png',
+      alt: 'Москва 24',
+    },
+    {
+      img: 'forbes.png',
+      alt: 'Forbes',
+    },
+  ],
+  [
+    {
+      img: 'russia-1.png',
+      alt: 'Россия 1',
+    },
+    {
+      alt: 'Все публикации',
+    },
+  ]
+])
+
+const setSlide = (i) => {
+  currentSlideIndex.value = i
+  swiperContainerTemplateRef.value.swiper.slideTo(currentSlideIndex.value)
+}
+
+const onSlideChanged = () => {
+  currentSlideIndex.value = swiperContainerTemplateRef.value.swiper.activeIndex
+}
+
+onMounted(async() => {
+  if (window.innerWidth < 768) {
+    isMobile.value = true
+    await nextTick()
+    swiperContainerTemplateRef.value.style.width = window.getComputedStyle(h2TemplateRef.value).width
+  }
+})
 </script>
 
 <template>
   <section class="talk-about-us">
-    <h2>О нас говорят</h2>
+    <h2 ref="h2TemplateRef">О нас говорят</h2>
     <div class="talk-about-us__resources">
-      <div v-for="mediaResource in mediaResources">
-        <div v-if="mediaResource.isWithTag" class="talk-about-us__resource-tag">
-          <TagWithLabel class="--alternative-rotate" icon="heart-search">#мы в сми</TagWithLabel>
+      <template v-if="isMobile">
+        <swiper-container ref="swiperContainerTemplateRef" @swiperslidechange="onSlideChanged" :spaceBetween="16" :slides-per-view="1.1">
+          <swiper-slide v-for="mediaResourcePart in mediaResourcesMobile">
+            <div class="talk-about-us__resource-slide">
+              <div class="talk-about-us__resource-card-mobile" v-for="mediaResource in mediaResourcePart">
+                <img v-if="mediaResource.img" :src="'/img/media/' + mediaResource.img" :alt="mediaResource.alt">
+                <Link v-else class="--contrast-on-hover" link="/">Все публикации</Link>
+              </div>
+            </div>
+          </swiper-slide>
+        </swiper-container>
+        <div class="talk-about-us__resources-slider-pagination">
+          <div @click="setSlide(i)" :class="{'--current': currentSlideIndex === i}" v-for="(_, i) in mediaResourcesMobile" />
         </div>
-        <img :src="'/img/media/' + mediaResource.img" :alt="mediaResource.alt">
-      </div>
-      <div>
-        <Link class="--contrast-on-hover" link="/">Все публикации</Link>
+      </template>
+      <div v-else class="talk-about-us__resource-card" v-for="mediaResource in mediaResources">
+        <template v-if="mediaResource.img">
+          <div v-if="mediaResource.isWithTag" class="talk-about-us__resource-tag">
+            <TagWithLabel class="--alternative-rotate" icon="heart-search">#мы в сми</TagWithLabel>
+          </div>
+          <img :src="'/img/media/' + mediaResource.img" :alt="mediaResource.alt">
+        </template>
+        <Link v-else class="--contrast-on-hover" link="/">Все публикации</Link>
       </div>
     </div>
   </section>

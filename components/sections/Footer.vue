748bdc62a1408a658d789131bde4f600bbb3fe47
Denis Danilov <d.danilov@duotek.ru>
2025-08-13 15:34:21 +0300

    Temp

--- components/sections/Footer.vue
+++ components/sections/Footer.vue
@@ -50,7 +50,8 @@ onMounted(() => {
 <template>
   <section class="footer">
     <div class="footer__container">
-      <div class="footer__line --with-gap">
+      <!--  desktop  -->
+      <div class="footer__line --with-gap --only-desktop">
         <div class="footer__column --logo">
           <div class="footer__logo-container">
             <LogoAlt />
@@ -73,8 +74,10 @@ onMounted(() => {
         </div>
         <div class="footer__column --contacts">
           <div><a class="footer__link --underline-on-hover" @pointerdown="copyEmailToClipboard" @contextmenu.prevent>{{ config.public.email }}</a></div>
-          <div><a class="footer__link --underline" target="_blank" :href="config.public.whatsapp">Написать в WhatsApp</a></div>
-          <div><a class="footer__link --underline" target="_blank" :href="config.public.telegram">Написать в Telegram</a></div>
+          <div class="footer__social">
+            <div><a class="footer__link --underline" target="_blank" :href="config.public.whatsapp">Написать в WhatsApp</a></div>
+            <div><a class="footer__link --underline" target="_blank" :href="config.public.telegram">Написать в Telegram</a></div>
+          </div>
         </div>
         <div class="footer__column --address">
           <div class="footer__text --primary --no-wrap">{{ config.public.legal.address }}</div>
@@ -85,7 +88,7 @@ onMounted(() => {
           </div>
         </div>
       </div>
-      <div class="footer__line">
+      <div class="footer__line --only-desktop">
         <div class="footer__column">
           <div class="footer__text --no-wrap">{{ config.public.legal.details}}</div>
         </div>
@@ -97,6 +100,49 @@ onMounted(() => {
         </div>
         <div class="footer__column">
           <a class="footer__link --secondary --contrast-on-hover" target="_blank" href="/docs/user-agreement.pdf">Пользовательское соглашение</a>
+        </div>
+      </div>
+      <!--  mobile  -->
+      <div class="footer__line --with-gap --only-mobile">
+        <div class="footer__column --logo">
+          <div class="footer__logo-container">
+            <LogoAlt />
+            <div>Digital Dyatel</div>
+          </div>
+        </div>
+        <div class="footer__column --contacts-and-social">
+          <div><a class="footer__link --underline" @click="callMeModal()">Позвоните мне</a></div>
+          <div class="footer__grid">
+            <div class="footer__phone" v-for="phone in phones">
+              <a class="footer__link --underline-on-hover" @pointerdown="clickOnPhone(phone)" @contextmenu.prevent>{{ phone.phone }}</a>
+              <div>{{ phone.description }}</div>
+            </div>
+            <div><a class="footer__link --underline" target="_blank" :href="config.public.whatsapp">Написать в WhatsApp</a></div>
+            <div><a class="footer__link --underline" target="_blank" :href="config.public.telegram">Написать в Telegram</a></div>
+          </div>
+          <div><a class="footer__link --underline-on-hover" @pointerdown="copyEmailToClipboard" @contextmenu.prevent>{{ config.public.email }}</a></div>
+        </div>
+        <div class="footer__column --address">
+          <div class="footer__text --primary --no-wrap">{{ config.public.legal.address }}</div>
+          <div class="footer__text">Режим работы: {{ config.public.legal.openingHours }}</div>
+          <div class="footer__metro">
+            <svg><use :href="'/sprite.svg#metro'" /></svg>
+            <div><span v-for="_metro in metro">{{ _metro }}</span></div>
+          </div>
+        </div>
+      </div>
+      <div class="footer__line --only-mobile">
+        <div class="footer__column">
+          <a class="footer__link --secondary --contrast-on-hover" target="_blank" href="/docs/offer.pdf">Оферта</a>
+        </div>
+        <div class="footer__column">
+          <a class="footer__link --secondary --contrast-on-hover" target="_blank" href="/docs/privacy-policy.pdf">Политика конфиденциальности</a>
+        </div>
+        <div class="footer__column">
+          <a class="footer__link --secondary --contrast-on-hover" target="_blank" href="/docs/user-agreement.pdf">Пользовательское соглашение</a>
+        </div>
+        <div class="footer__column">
+          <div class="footer__text">{{ config.public.legal.details}}</div>
         </div>
       </div>
     </div>
